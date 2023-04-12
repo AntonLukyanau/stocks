cefd10dc67d92fcc8c695fa6e471141502dbf76d
Anton_Lukyanau1 <anton_lukyanau1@epam.com>
2023-04-12 21:50:33 +0200

    add checkstyle and resolve checkstyle issues

--- src/main/java/com/alukyanau/nysestocks/infrastructure/cache/LRuCacheMap.java
+++ src/main/java/com/alukyanau/nysestocks/infrastructure/cache/LRuCacheMap.java
@@ -7,7 +7,7 @@ class LRuCacheMap<K, V> extends LinkedHashMap<K, V> {
 
     private final int maxSize;
 
-    public LRuCacheMap(int maxCacheSize) {
+    LRuCacheMap(int maxCacheSize) {
         super(maxCacheSize, 0.75f, true);
         this.maxSize = maxCacheSize;
     }
